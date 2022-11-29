4f5a71924f1a31d78d4c549c1c706a48d4156878
Melker Holmgren <melker@funnel.io>
2022-11-29 17:28:39 +0100

    Advent of code: solved second part of day 3

--- 2015/p3/main.py
+++ 2015/p3/main.py
@@ -36,4 +36,34 @@ with open("data", "r") as file:
     directions = file.readline()
 
 
-print(result_of_present_delivery(directions))
+def result_of_delivery_robo_and_santa(route):
+    houses_visited = {"0,0": 1}
+    snt_x = 0
+    snt_y = 0
+    rob_x = 0
+    rob_y = 0
+
+    for index, direction in enumerate(route):
+        if index % 2 == 0:
+            snt_x, snt_y = receive_coordinates(direction, snt_x, snt_y)
+            snt_house = f"{snt_x},{snt_y}"
+            check_if_house_got_a_visit(snt_house, snt_x, snt_y, houses_visited)
+        else:
+            rob_x, rob_y = receive_coordinates(direction, rob_x, rob_y)
+            rob_house = f"{rob_x},{rob_y}"
+            check_if_house_got_a_visit(rob_house, rob_x, rob_y, houses_visited)
+
+    return len(houses_visited), count_delivered_presents(houses_visited)
+
+
+def check_if_house_got_a_visit(house, x, y, houses_visited):
+    if house in houses_visited:
+        houses_visited[house] += 1
+    else:
+        houses_visited.update({house: 1})
+
+
+print(f"Only santa: {result_of_present_delivery(directions)}")
+print(f"Santa and Robo-Santa{result_of_delivery_robo_and_santa(directions)}")
+
+
--- 2015/p3/test_main.py
+++ 2015/p3/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest import TestCase
-from main import result_of_present_delivery, receive_coordinates
+from main import result_of_present_delivery, receive_coordinates, result_of_delivery_robo_and_santa
 
 
 def test_result_of_present_delivery_should_return_two_houses_visited_and_two_presents_delivered():
@@ -35,4 +35,24 @@ def test_receive_coordinates():
     assert actual_north == (0, 1)
     assert actual_south == (0, -1)
 
+    # ^v delivers presents to 3 houses, because Santa goes north, and then Robo - Santa goes south.
+    # ^>v< now delivers presents to 3 houses, and Santa and Robo - Santa end up back where they started.
+    # ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
 
+
+def test_result_delivers_presents_to_3_houses_because_santa_goes_north_and_then_robo_santa_goes_south():
+    data = "^v"
+    actual = result_of_delivery_robo_and_santa(data)
+    assert actual == (3, 3)
+
+
+def test_result_now_delivers_presents_to_3_houses_and_santa_and_robo_santa_end_up_back_where_they_started():
+    data = "^>v<"
+    actual = result_of_delivery_robo_and_santa(data)
+    assert actual == (3, 5)
+
+
+def test_result_now_delivers_presents_to_11_houses_with_santa_going_one_direction_and_robo_santa_going_the_other():
+    data = "^v^v^v^v^v"
+    actual = result_of_delivery_robo_and_santa(data)
+    assert actual == (11, 11)
