fad4e13a614766252b6c31b069bad22a1b06bf06
Melker Holmgren <melker@funnel.io>
2022-11-29 16:31:56 +0100

    Advent of code: solved fitst part of day 3

--- 2015/p3/main.py
+++ 2015/p3/main.py
@@ -1,17 +1,39 @@
 def result_of_present_delivery(route):
+    houses_visited = {"0,0": 1}
+    x = 0
+    y = 0
+
     for direction in route:
-        
+        x, y = receive_coordinates(direction, x, y)
+        house = f"{x},{y}"
+        if house in houses_visited:
+            houses_visited[house] += 1
+        else:
+            houses_visited.update({house: 1})
+    return len(houses_visited), count_delivered_presents(houses_visited)
 
 
+def receive_coordinates(direction, x, y):
+    if direction == ">":
+        x += 1
+    if direction == "<":
+        x -= 1
+    if direction == "^":
+        y += 1
+    if direction == "v":
+        y -= 1
+    return x, y
 
-class House:
-    def __init__(self, x, y, presents=1):
-        self.x = x
-        self.y = y
-        self.presents = presents
 
-    def get_house_coordinates(self):
-        return self.x, self.y
+def count_delivered_presents(houses_visited):
+    number_of_presents = 0
+    for key in houses_visited:
+        number_of_presents += houses_visited[key]
+    return number_of_presents
 
-    def deliver_present(self, number):
-        self.presents += number
+
+with open("data", "r") as file:
+    directions = file.readline()
+
+
+print(result_of_present_delivery(directions))
--- 2015/p3/test_main.py
+++ 2015/p3/test_main.py
@@ -1,4 +1,6 @@
-from main import result_of_present_delivery
+import pytest
+from unittest import TestCase
+from main import result_of_present_delivery, receive_coordinates
 
 
 def test_result_of_present_delivery_should_return_two_houses_visited_and_two_presents_delivered():
@@ -17,5 +19,20 @@ def test_result_of_present_delivery_should_return_two_houses_visited_and_eleven_
     data = "^v^v^v^v^v"
     actual = result_of_present_delivery(data)
     assert actual == (2, 11)
+
+
+def test_receive_coordinates():
+    data_east = ">"
+    data_west = "<"
+    data_north = "^"
+    data_south = "v"
+    actual_east = receive_coordinates(data_east, 0, 0)
+    actual_west = receive_coordinates(data_west, 0, 0)
+    actual_north = receive_coordinates(data_north, 0, 0)
+    actual_south = receive_coordinates(data_south, 0, 0)
+    assert actual_east == (1, 0)
+    assert actual_west == (-1, 0)
+    assert actual_north == (0, 1)
+    assert actual_south == (0, -1)
 
 
